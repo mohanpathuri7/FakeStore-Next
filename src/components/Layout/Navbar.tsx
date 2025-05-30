c38811f32f07c235a1f7cc0cd1bebb95de8082a1
Mohan Pradeep Kumar P <mohan.pradeep@valtech.com>
2025-05-31 05:28:02 +0530

    update code 1

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -22,17 +22,6 @@ const Navbar: React.FC = () => {
                     </Link>
                 </nav>
             </div>
-
-            <div className="md:hidden px-4 pb-4">
-                <div className="relative w-full">
-                    <input
-                        type="text"
-                        placeholder="Search products..."
-                        className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                    <Search className="absolute left-3 top-2.5 text-gray-400 w-5 h-5" />
-                </div>
-            </div>
         </header>
     )
 }
