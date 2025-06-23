10506ce5fc58232336a2d19383f556cef2d5c1b1
Mohan Pradeep Kumar P <mohan.pradeep@valtech.com>
2025-06-23 21:09:22 +0530

    page active

--- src/app/(main)/components/Layout/Navbar.tsx
+++ src/app/(main)/components/Layout/Navbar.tsx
@@ -3,22 +3,30 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from 'next/link'
 import { ShoppingCart } from 'lucide-react';
 import { useCartStore } from '@/app/(main)/store/cartStore';
+import { usePathname } from 'next/navigation';
 
 
 const Navbar: React.FC = () => {
     const { totalItems } = useCartStore();
+    const pathname = usePathname();
     return (
         <header className="bg-white shadow-md sticky top-0 z-10">
             <div className="container mx-auto px-4 py-4 flex items-center justify-between">
                 <Link href="/" className="text-2xl font-bold text-blue-600 flex items-center">
                     <span className="ml-2">FakeStore</span>
                 </Link>
-                <nav className="flex items-center space-x-6">
-                    <Link href="/products" className="text-gray-700 hover:text-blue-600 font-medium">
+                <nav className="flex items-center space-x-4">
+                    <Link href="/products" className={`font-medium ${pathname === '/products'
+                        ? 'text-blue-600 font-semibold'
+                        : 'text-gray-700 hover:text-blue-600'
+                        }`}>
                         Products
                     </Link>
                     <SignedIn>
-                        <Link href="/order" className="text-gray-700 hover:text-blue-600 font-medium">
+                        <Link href="/order" className={`font-medium ${pathname === '/order'
+                            ? 'text-blue-600 font-semibold'
+                            : 'text-gray-700 hover:text-blue-600'
+                            }`}>
                             Orders
                         </Link>
                     </SignedIn>
